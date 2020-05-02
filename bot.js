5bbc87e772c646597e821e177524ecc1fffa1a54
RadAngel <radangel13338@gmail.com>
2020-05-02 15:45:33 -0500

    Test for local .env and Heroku .env implementation

--- bot.js
+++ bot.js
@@ -1,6 +1,8 @@
+require('dotenv').config({ path: '.env' })
 const botsettings = process.env;
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const ytdl = require('ytdl-core-discord');
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -10,6 +12,8 @@ client.on('message', msg => {
   if (msg.content === 'ping') {
     msg.reply('pong');
   }
+  if (msg.content == botsettings.prefix + 'play') {
+      console.log(msg.member.voice.channel);
+  }
 });
-
-client.login(botsettings.token);
+client.login(botsettings.TOKEN);
