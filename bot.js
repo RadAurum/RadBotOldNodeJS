10024cfd9c809b76f81adaccb6bffe52fed988c7
RadAngel <radangel13338@gmail.com>
2020-05-02 23:01:56 -0500

    Audio test
    ya estoy hasta la verga loco

--- bot.js
+++ bot.js
@@ -14,7 +14,22 @@ client.on('message', msg => {
     msg.reply('pong');
   }
   if (msg.content == prefix + 'play') {
-      console.log(msg.member.voice.channel);
+    console.log('playing');
+    // console.log(msg.member.voice.channel);
+    if(msg.member.voice.channel) {
+      console.log('user in voice channel')
+      msg.member.voice.channel.join().then(async c => {
+        console.log(c);
+        console.log("Reproduciendo goteo");
+        dispatcher = c.play(await ytdl("https://www.youtube.com/watch?v=AaHixdQS2jA"), {volume: 1.0,type: 'opus'});
+        dispatcher.on("error", e => {console.log(e)});
+        dispatcher.on("start", s => {console.log("start goteo");console.log(s)});
+        console.log("estoy que goteo");
+      });
+    }
+  }
+  if (msg.content == prefix + 'stop') {
+    msg.guild.voice.connection.disconnect();
   }
 });
 client.login(botsettings.TOKEN);
